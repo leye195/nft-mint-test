faea1bb2faf2e9e6bdac9551c7c6a9d51e50379d
leye195 <leye195@naver.com>
2023-05-04 23:24:34 +0900

    feat: add MintToken contract

---
+++ contracts/MintToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract MintToken is ERC721Enumerable {
+    constructor() ERC721("testTokens","TTS") {}
+
+    mapping(uint256 => uint256) public tokenTypes;
+
+    function mintTestToken() public {
+        uint256 tokenId = totalSupply() + 1; // mint id
+
+        // random 값 추출
+        uint256 tokenType = uint256(keccak256(abi.encodePacked(block.timestamp,msg.sender,tokenId))) % 5 + 1;
+
+        tokenTypes[tokenId] = tokenType;
+
+        _mint(msg.sender,tokenId); // mint function suppled by ERC721
+    }
+}
+
+ 
