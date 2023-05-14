748837a04bc2d363498bafa9d03669f810ed627d
leye195 <leye195@naver.com>
2023-05-14 21:20:59 +0900

    feat: add contracts to export contract address,abi

---
+++ frontend/src/lib/contracts/index.ts
@@ -0,0 +1,10 @@
+import mintTokenAbi from "./mintToken.json";
+import saleTokenAbi from "./saleToken.json";
+
+export const mintTokenAddress = "0xDDBb3C201FFb00e33AAA46DC310b38A32bc451e8";
+export const saleTokenAddress = "0x004E347Cb52314937d99dAdE20557Cc7297F4437";
+
+export default {
+  mintTokenAbi,
+  saleTokenAbi,
+};
