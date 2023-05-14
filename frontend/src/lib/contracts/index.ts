aa90dda79f4d97475fbefd50660dfe2ccdde9e37
leye195 <leye195@naver.com>
2023-05-14 23:57:24 +0900

    feat: define type to Address

--- frontend/src/lib/contracts/index.ts
+++ frontend/src/lib/contracts/index.ts
@@ -1,8 +1,11 @@
-import mintTokenAbi from "./mintToken.json";
-import saleTokenAbi from "./saleToken.json";
+import { Address } from "wagmi";
+import mintTokenAbi from "./mintToken";
+import saleTokenAbi from "./saleToken";
 
-export const mintTokenAddress = "0xDDBb3C201FFb00e33AAA46DC310b38A32bc451e8";
-export const saleTokenAddress = "0x004E347Cb52314937d99dAdE20557Cc7297F4437";
+export const mintTokenAddress: Address =
+  "0xDDBb3C201FFb00e33AAA46DC310b38A32bc451e8";
+export const saleTokenAddress: Address =
+  "0x004E347Cb52314937d99dAdE20557Cc7297F4437";
 
 export default {
   mintTokenAbi,
