0d0d11b9f0883257df919e1c7689f001f081e591
leye195 <leye195@naver.com>
2023-05-20 16:28:31 +0900

    feat: add MyToken page

---
+++ frontend/src/routes/MyToken.tsx
@@ -0,0 +1,12 @@
+import Flex from "@/components/Flex";
+import SectionWithLabel from "@/components/SectionWithLabel";
+
+function MyToken() {
+  return (
+    <Flex flexDirection="column" width="100%" padding="12px">
+      <SectionWithLabel label="My Token"></SectionWithLabel>
+    </Flex>
+  );
+}
+
+export default MyToken;
