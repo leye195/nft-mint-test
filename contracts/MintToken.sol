0cfc0f42c1e5d2d7769365ef76bcbd3887d9705b
leye195 <leye195@naver.com>
2023-05-29 11:24:35 +0900

    feat: add  setSaleToken set onlyOwner on function

--- contracts/MintToken.sol
+++ contracts/MintToken.sol
@@ -2,12 +2,23 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MintToken is ERC721Enumerable {
+import "SaleToken.sol";
+
+contract MintToken is ERC721Enumerable, Ownable {
     constructor() ERC721("testTokens","TTS") {}
 
+    SaleToken public saleToken;
+
     mapping(uint256 => uint256) public tokenTypes;
+
+    struct TokenData {
+        uint256 tokenId;
+        uint256 tokenType;
+        uint256 price;
+    }
 
     function mintTestToken() public {
         uint256 tokenId = totalSupply() + 1; // mint id
@@ -19,6 +30,12 @@ contract MintToken is ERC721Enumerable {
         tokenTypes[tokenId] = tokenType;
 
         _mint(msg.sender,tokenId); // mint function suppled by ERC721
+    }
+
+
+
+    function setSaleToken(address _saleToken) public onlyOwner {
+        saleToken = SaleToken(_saleToken);
     }
 }
 
