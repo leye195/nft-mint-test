a64f1ef3455c1e0bcb3b428149fac3f3bcd9f6c2
leye195 <leye195@naver.com>
2023-05-29 21:17:52 +0900

    feat: update deploy script

--- contracts/migrations/1_deploy.js
+++ contracts/migrations/1_deploy.js
@@ -1,5 +1,10 @@
 const Mint = artifacts.require("MintToken");
+const Sale = artifacts.require("SaleToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(Mint);
+module.exports = async function (deployer) {
+  await deployer.deploy(Mint);
+  const mint = await Mint.deployed();
+
+  await deployer.deploy(Sale, mint.address);
+  // await deployer.deploy(Sale(mint.address));
 };
